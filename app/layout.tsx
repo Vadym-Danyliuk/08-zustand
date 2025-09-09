bb9f9313197ee2a98d6f98aa9a8eefd6f759e7db
Vadym Danyliuk <jekone13@outlook.com>
2025-09-09 12:07:54 +0300

    8,3

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
+  
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
+ 
 }>) {
   return (
     <html lang="uk" className={roboto.variable}>
@@ -39,7 +39,7 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           {children}
-          {modal}
+          
           <Footer />
         </TanStackProvider>
       </body>
